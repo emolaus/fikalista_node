674f52e332c2fc20fa494ccf9d707c618646cc94
emolaus <bakaren@gmail.com>
2016-04-14 15:01:51 +0000

    First api route working

--- routes/restapi.js
+++ routes/restapi.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('data/database.db');
+console.log('restapi cached db');
+
+router.get('/:groupurl/users', function(req, res, next) {
+  res.send(JSON.stringify({name: 'Kalle'}));
+});
+router.get('/test', function (req, res, next) {
+  db.all('SELECT * FROM groups;', function (err, data) {
+    if (err) {
+      console.log('Error retrieving data!');
+      res.send(err);
+    }
+    else {
+      res.send(data);
+    }
+  });
 });
 
 module.exports = router;
