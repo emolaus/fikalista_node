8f6c2d75f9e59cb3d9ae50c0f3f49e7d69e2c527
emolaus <bakaren@gmail.com>
2016-04-14 15:19:37 +0000

    Second route added: /restapi/users/:groupurl

--- routes/restapi.js
+++ routes/restapi.js
@@ -16,5 +16,31 @@ router.get('/groups', function (req, res, next) {
     }
   });
 });
+router.get('/users/:groupurl', function (req, res, next) {
+  // Check so that groupurl exists
+
+  var statement = db.prepare("SELECT * FROM groups WHERE url_name=?", req.params.groupurl);
+  statement.bind(req.params.groupurl);
+  statement.all(function (err, rows) {
+    if (err) {
+      console.log('error reading db in route /users/:groupurl');
+      res.send('Error reading db');
+      return;
+    }
+    if (rows.length == 0) {
+      res.send('group not found.');
+      return;
+    }
+    // Get all users of that group or return error
+    db.all('SELECT * FROM users where groupid=' + rows[0].groupid, function (err, rows) {
+      if (err) {
+        console.log('Error reading db table users in route /users/:groupurl');
+        res.send('Error fetching users.');
+        return;
+      }
+      res.send(rows);
+    });
+  });
+});
 
 module.exports = router;
