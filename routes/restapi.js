aab70bed8ecf89b34f520b4e54aefcdda0a3e3c8
emolaus <bakaren@gmail.com>
2016-04-14 15:02:41 +0000

    First api route renamed to restapi/groups

--- routes/restapi.js
+++ routes/restapi.js
@@ -5,10 +5,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('data/database.db');
 console.log('restapi cached db');
 
-router.get('/:groupurl/users', function(req, res, next) {
-  res.send(JSON.stringify({name: 'Kalle'}));
-});
-router.get('/test', function (req, res, next) {
+router.get('/groups', function (req, res, next) {
   db.all('SELECT * FROM groups;', function (err, data) {
     if (err) {
       console.log('Error retrieving data!');
