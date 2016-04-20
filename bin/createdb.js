bd3a8c0f32e11da103bba01ebd4eca79543342ec
emolaus <bakaren@gmail.com>
2016-04-20 08:43:56 +0000

    Changed db structure. Removed groupid and set groupurl as primary key.

--- bin/createdb.js
+++ bin/createdb.js
@@ -2,15 +2,15 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('data/database.db');
 
 db.run("CREATE TABLE groups (" + 
-        "groupid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," + 
+        //"groupid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," + 
+        "groupurl TEXT NOT NULL PRIMARY KEY UNIQUE," +
         "name TEXT NOT NULL," +
-        "groupurl TEXT NOT NULL UNIQUE," +
         "pincode TEXT DEFAULT '0000'," + 
         "creation_time INTEGER DEFAULT CURRENT_TIMESTAMP NOT NULL" +
         ")");
 db.run("CREATE TABLE users (" + 
         "userid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
-        "groupid INTEGER NOT NULL," + 
+        "groupurl TEXT NOT NULL," + 
         "name TEXT NOT NULL," +
         "user_order INTEGER DEFAULT 0," + // "order" protected keyword...
         "fika_count INTEGER DEFAULT 0," +
@@ -21,13 +21,8 @@ db.run("CREATE TABLE users (" +
         
 db.run("CREATE TABLE skipweeks (" + 
         "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
-        "groupid INTEGER NOT NULL," +
-        "week_number INTEGER NOT NULL," +
+        "groupurl TEXT NOT NULL," +
+        "week INTEGER NOT NULL," +
         "year INTEGER NOT NULL" +
         ")");
-/* TODO Tables
-Users			
-userid	name	group	order
-Skipweeks			
-groupid	year	week	
-*/
+        
